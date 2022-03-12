162b264787a9a60aedbb0a190add64b61095b85f
Grant Jenks <grant.jenks@gmail.com>
2022-03-12 12:14:55 -0800

    flake8 fixes

--- dblog/__init__.py
+++ dblog/__init__.py
@@ -3,9 +3,10 @@
 
 from .handlers import DBLogHandler
 
+__all__ = ['DBLogHandler']
+
 __title__ = 'dblog'
 __version__ = '0.2.1'
-__build__ = 0x000201
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
-__copyright__ = '2018-2019, Grant Jenks'
+__copyright__ = '2018-2022, Grant Jenks'
