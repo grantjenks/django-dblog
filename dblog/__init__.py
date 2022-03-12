c064b8018b4221ee7d6260cf6430b7cfa8dccee8
Grant Jenks <grant.jenks@gmail.com>
2022-03-12 13:25:37 -0800

    Bump version to 0.3.0

--- dblog/__init__.py
+++ dblog/__init__.py
@@ -6,7 +6,7 @@ from .handlers import DBLogHandler
 __all__ = ['DBLogHandler']
 
 __title__ = 'dblog'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = '2018-2022, Grant Jenks'
